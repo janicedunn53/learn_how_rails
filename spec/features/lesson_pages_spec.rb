8e5cb535facd89261eaa0528f30b566ebae5dd3a
Brigham Thompson & Janice Dunn <student@epicodus.com>
2015-10-13 11:26:40 -0700

    add spec for testing edit path

--- spec/features/lesson_pages_spec.rb
+++ spec/features/lesson_pages_spec.rb
@@ -17,3 +17,14 @@ describe "the add a lesson process" do
     expect(page).to have_content 'errors'
   end
 end
+
+describe "the edit lesson path" do
+  it "edits a lesson" do
+    lesson = Lesson.create(name: "CSS", content: "CSS stuff", number: 9)
+    visit lesson_path(lesson)
+    click_on "Edit"
+    fill_in "Number", :with => 12
+    click_on "Update Lesson"
+    expect(page).to have_content "Lessons"
+  end
+end
