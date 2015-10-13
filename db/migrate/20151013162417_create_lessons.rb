2bb322a755e204de9885887f027f2faf21e85b00
Brigham Thompson & Janice Dunn <student@epicodus.com>
2015-10-13 09:38:44 -0700

    add lesson class and passing spec

---
+++ db/migrate/20151013162417_create_lessons.rb
@@ -0,0 +1,11 @@
+class CreateLessons < ActiveRecord::Migration
+  def change
+    create_table :lessons do |t|
+      t.column :name, :string
+      t.column :content, :string
+      t.column :number, :integer
+
+      t.timestamps
+    end
+  end
+end
